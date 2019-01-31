af59a887c6f23c8f7d705fd09e3ae8ff23999d3d
yangleiyanglei <yangleiyanglei@didichuxing.com>
2019-01-31 14:10:23 +0800

    React-Navigation & Redux

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './src/reducers';
+import { initTasks } from './src/reducers/action';
+
+import { RoutersConfig } from './App';
+
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+const AppNavigator = createStackNavigator(RoutersConfig.routes, RoutersConfig.initial);
+
+const store = createStore(reducer/*, applyMiddleware()*/);
+
+const AppContaienr = createAppContainer(AppNavigator);
+
+function Application() {
+    return (
+        <Provider store={store}>
+            <AppContaienr />
+        </Provider>
+    );
+}
+
+store.dispatch(initTasks());
+
+AppRegistry.registerComponent(appName, () => Application);
